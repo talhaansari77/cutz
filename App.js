be36c15d4b10a2207e8a26524207f503f5251c4a
talhaansari77 <talhaprince09@gmail.com>
2023-03-04 18:39:22 +0300

    structure

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, LogBox } from "react-native";
+import RootNavigator from "./src/routes";
+import { useFonts } from "expo-font";
+LogBox.ignoreLogs(["VirtualizedLists", "Warning:..."]);
+LogBox.ignoreAllLogs();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [loaded] = useFonts({
+    regular: require("./assets/Fonts/Roboto-Regular.ttf"),
+    bold: require("./assets/Fonts/Roboto-Bold.ttf"),
+    light: require("./assets/Fonts/Roboto-Light.ttf"),
+    semiBold: require("./assets/Fonts/Roboto-Medium.ttf"),
+  });
+
+  if (!loaded) return <View />;
+
+  return <RootNavigator />;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ src/utils/Colors.js
@@ -0,0 +1,16 @@
+export const colors = {
+  primary: "#F6AD00",
+  black:"black",
+  lightWhite: "#EBEBF599",
+  orange: "#FB9334",
+  gray: "#8D8D8D",
+  gray1: "#B9B7B7",
+  white: "#FFFFFF",
+  red: "red",
+  blue: "blue",
+  lightGray: "#e9ecef",
+  halfGray: "#9F9F9F",
+  lightBlack:"#6c757d",
+  lightBlack1:"#495057",
+  lightOrange:"#fdeecc"
+};
